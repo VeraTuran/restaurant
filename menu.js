6141c8ed5344b1fcc8626cea07e481c2fe9c860b
VeraTuran <vmeturan@gmail.com>
2021-07-19 16:56:06 +0200

    popup added, randomisation works

--- menu.js
+++ menu.js
@@ -1,21 +1,44 @@
-var main_courses = ["Filet de turbot de la mer Noire", "Tablier de sapeur", "Gigot d'agneau", "Faisan de forêt", "Trio de quinoa, chou kale et pousses d'épinard"]
-var techniques = ["à la cocotte", "minute", "avec sa sauce hollandaise", "façon sud-ouest", "comme chez ma grand-mère", "déglacé au saké", "maturé en fût de chêne"]
-var sides = ["une purée de topinambour", "ses frites truffées", "des châtaignes croustillantes", "une brunoise carotte-cèleri", "un oeuf parfait", "sa crème veloutée de fromages affinés"]
-var seasonings = ["au yuzu rouge", "au poivre vert de Sichuan", "et une pointe de safran", "à l'ail noir", "et un peu de sucre en poudre"]
-
-var random_main_course = main_courses[Math.floor(Math.random()*main_courses.length)]
-var random_technique = techniques[Math.floor(Math.random()*techniques.length)]
-var random_side = sides[Math.floor(Math.random()*sides.length)]
-var random_seasoning = seasonings[Math.floor(Math.random()*seasonings.length)]
-
-var menu = `${random_main_course} ${random_technique}, avec ${random_side} ${random_seasoning}`
-
 const myChgMenu = () => {
-    document.getElementById('fixMenu').innerHTML = menu();
+    document.getElementById("chgButton").addEventListener('click', (e) => {   
+        var main_courses = ["Filet de turbot de la mer Noire", "Tablier de sapeur", "Gigot d'agneau", "Faisan de forêt", "Trio de quinoa, chou kale et pousses d'épinard"]
+        var techniques = ["à la cocotte", "minute", "avec sa sauce hollandaise", "façon sud-ouest", "comme chez ma grand-mère", "déglacé au saké", "maturé en fût de chêne"]
+        var sides = ["une purée de topinambour", "ses frites truffées", "des châtaignes croustillantes", "une brunoise carotte-cèleri", "un oeuf parfait", "sa crème veloutée de fromages affinés"]
+        var seasonings = ["au yuzu rouge", "au poivre vert de Sichuan", "et une pointe de safran", "à l'ail noir", "et un peu de sucre en poudre"]
+        
+        var random_main_course = main_courses[Math.floor(Math.random()*main_courses.length)]
+        var random_technique = techniques[Math.floor(Math.random()*techniques.length)]
+        var random_side = sides[Math.floor(Math.random()*sides.length)]
+        var random_seasoning = seasonings[Math.floor(Math.random()*seasonings.length)]
+        
+        var menu = `${random_main_course} ${random_technique}, avec ${random_side} ${random_seasoning}`
+        console.log(e);
+    e.preventDefault();
+    document.getElementById('fixMenu').innerHTML = menu;
+    });
 };
 
-const chgMenu = document.getElementById('fixMenu').addEventListener('click', () => {
-    myChgMenu();
-});    
+document.addEventListener('DOMContentLoaded', () => {
+    myChgMenu();   
+});
+
+function onMouseOut(event) {
+    // If the mouse is near the top of the window, show the popup
+    if (event.clientY < 50) {
+      // Remove this event listener
+      document.removeEventListener("mouseout", onMouseOut);
+  
+      // Show the popup
+      document.getElementById("popup").style.display = "block";
+    }
+  }
+  
+  document.addEventListener("mouseout", onMouseOut);
 
 //bouton avec event listener onlick "changer le menu"
+
+document.addEventListener("mouseleave", function(e){
+    if( e.clientY < 0 )
+    {
+         alert("Merci de nous visiter dans le restaurant qui vit en 3021. ;)");
+    }
+}, false);
